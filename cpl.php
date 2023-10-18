284e0c262a0ebe29e1fa062fc2b582150b29cf8f
floren-audrina <c14210225@john.petra.ac.id>
2023-10-18 13:05:13 +0700

    minor

--- cpl.php
+++ cpl.php
@@ -12,5 +12,33 @@
     <body>
         <!-- navbar -->
         <?php include "navbar/navbar_after_login.php";?>
+        <!-- bread crumbs -->
+        <!-- isi -->
+        <div class="container overflow-hidden text-center">
+            <div class="row gy-5">
+                <div class="col-6">Periode:</div>
+                <div class="col-6">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Select an option
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">All</a></li>
+                            <li><a class="dropdown-item" href="#">Gasal</a></li>
+                            <li><a class="dropdown-item" href="#">Genap</a></li>
+                        </ul>
+                    </div>
+                </div>
+            <!-- </div> -->
+            <!-- <div class="row gy-5"> -->
+                <div class="col-6">Tahun:</div>
+                <div class="col-6"></div>
+            <!-- </div> -->
+            <!-- <div class="row gy-5"> -->
+                <div class="col-6">Angkatan:</div>
+                <div class="col-6"></div>
+            <!-- </div> -->
+</div>
+        </div>
     </body>
 </html>
