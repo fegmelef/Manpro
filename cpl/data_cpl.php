440ed36fa847a61b5818b0708ed7e5fec5f13394
floren-audrina <c14210225@john.petra.ac.id>
2023-10-20 20:05:22 +0700

    tabel data list + minor fix

--- cpl/data_cpl.php
+++ cpl/data_cpl.php
@@ -12,13 +12,12 @@ if (isset($_GET["tahun"])) {
 if (isset($_GET["periode"])) {
     $periode = $_GET['periode'];
 }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Welcome Page</title>
+        <title>Data CPL</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -63,7 +62,66 @@ if (isset($_GET["periode"])) {
                 </div>    
             </div>
             
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table">
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">NRP</th>
+                            <th scope="col">Tahun</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                            $query = $conn->prepare("SELECT * FROM mhsw");
+                            $query->execute();
+                            $rowNum = 1;
+                            while($row = $query->fetch()) {
+                                echo '<tr>
+                                <th scope="row">'.$rowNum.'</th>
+                                <td>'.$row['nama'].'</td>
+                                <td>'.$row['nrp_hash'].'</td>
+                                <td>'.$row['tahun'].'</td>
+                            </tr>';
+                            $rowNum++;
+                            }
+                        ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+                <!-- <div class="col-md-12">
+                    <table class="table">
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">NRP</th>
+                            <th scope="col">Tahun</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                        </tbody>
+                    </table> 
+                </div>
+            </div>                       -->                        
         </div>
-
     </body>
 </html>
--- ipk/home_ipk.php
+++ ipk/home_ipk.php
@@ -1,16 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Welcome Page</title>
+        <title>Home IPK</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="css.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+        <link rel="stylesheet" type="text/css" href="../css.css">
     </head>
     <body>
         <!-- navbar -->
+       
         <?php include "../navbar/navbar_after_login.php";?>
+
+        <!-- bread crumbs -->
+            <div class="row">
+            <ul id="breadcrumb" class="breadcrumb">
+                <li class="breadcrumb-item active">Home</li>
+            </ul>
+            </div>
+
+        <!-- isi -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4" style="font-size:30px;">Periode</div>
+                <div class="col-md-2">:</div>
+                <div class="col-md-6">
+                    <select name="periode" id="periode" class="form-control">
+                        <option value="none">Pilih periode</option>
+                        <option value="all">All</option>
+                        <option value="gasal">Gasal</option>
+                        <option value="genap">Genap</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4" style="font-size:30px;">Tahun</div>
+                <div class="col-md-2">:</div>
+                <div class="col-md-6">
+                    <select name="tahun" id="tahun" class="form-control">
+                        <option value="none">Pilih tahun</option>
+                        <option value="all">All</option>
+                        <option value="2019">2019</option>
+                        <option value="2020">2020</option>
+                        <option value="2021">2021</option>
+                        <option value="2022">2022</option>
+                        <option value="2023">2023</option>                    
+                    </select>
+                </div>
+            </div>
+            <div class="row" style="margin-bottom:50px;">
+                <div class="col-md-4" style="font-size:30px;">Angkatan</div>
+                <div class="col-md-2">:</div>
+                <div class="col-md-6">
+                    <select name="angkatan" id="angkatan" class="form-control">
+                        <option value="none">Pilih angkatan</option>
+                        <option value="all">All</option>
+                        <option value="2019">2019</option>
+                        <option value="2020">2020</option>
+                        <option value="2021">2021</option>
+                        <option value="2022">2022</option>
+                        <option value="2023">2023</option>                    
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-12 text-center"> <!-- Center the button within the column -->
+                    <button type="button" class="btn btn-lg"><a href="data_cpl.php" style="text-decoration: none;">Generate</a></button>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
