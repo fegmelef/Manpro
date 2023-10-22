415bb81b74a2d723818314694b9359a271d06696
Duevvv <nathaliadevita02@gmail.com>
2023-10-22 15:14:07 +0700

    Update reporting.php

--- cpl/reporting.php
+++ cpl/reporting.php
@@ -1,5 +1,17 @@
 <?php
-include ("../api/connect.php");
+include("../api/connect.php");
+
+// if (isset($_GET["angkatan"])) {
+//     $angkatan = $_GET['angkatan'];
+// }
+
+// if (isset($_GET["tahun"])) {
+//     $tahun = $_GET['tahun'];
+// }
+
+// if (isset($_GET["periode"])) {
+//     $periode = $_GET['periode'];
+// }
 ?>
 
 <!DOCTYPE html>
@@ -35,6 +47,16 @@ include ("../api/connect.php");
 
             
         <!-- isi -->
+        <!-- <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <p class="semester">Semester:
+                    <?php echo $periode; ?><br>Angkatan:
+                    <?php echo $angkatan; ?><br>Tahun:
+                    <?php echo $tahun; ?>
+
+                </p>
+            </div> -->
         <div class="container">
             <div class="row">
                 <div class="col-md-3">
@@ -46,6 +68,46 @@ include ("../api/connect.php");
                 </select>
                 </div>    
             </div>
+
+            <!-- <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                <select name="filtering-CPL" id="filtering-CPL" class="form-control1">
+                    <option value="TF-01">TF-01</option>                               
+                    <option value="TF-02">TF-02</option>                               
+                    <option value="TF-03">TF-03</option>                               
+                    <option value="TF-04">TF-04</option>                               
+                    <option value="TF-05">TF-05</option>                               
+                    <option value="TF-06">TF-06</option>                               
+                    <option value="TF-07">TF-07</option>                               
+                    <option value="TF-08">TF-08</option>                               
+                    <option value="TF-09">TF-09</option>                               
+                    <option value="TF-10">TF-10</option>                               
+                </select>
+                </div>    
+            </div> -->
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <form method="post" action="">
+                        <select name="filtering-CPL" id="filtering-CPL" class="form-control1">
+                            <option value="TF-01">TF-01</option>
+                            <option value="TF-02">TF-02</option>
+                            <option value="TF-03">TF-03</option>
+                            <option value="TF-04">TF-04</option>
+                            <option value="TF-05">TF-05</option>
+                            <option value="TF-06">TF-06</option>
+                            <option value="TF-07">TF-07</option>
+                            <option value="TF-08">TF-08</option>
+                            <option value="TF-09">TF-09</option>
+                            <option value="TF-10">TF-10</option>
+                        </select>
+                        <input type="submit" value="Kirim">
+                    </form>
+                </div>
+            </div>
+        </div>
             
             <div class="row">
                 <div class="col-md-12">
@@ -61,20 +123,48 @@ include ("../api/connect.php");
                         </thead>
                         <tbody>
                         <?php
-                            $query = $conn->prepare("SELECT kelas_nilaicpmk.*, ikcpl.id_ikcpl, mhsw.tahun, mhsw.nama
-                            FROM kelas_nilaicpmk
+                            $kode_cpl;
+                            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                                // Mengambil nilai dropdown yang dipilih
+                                $selectedValue = $_POST['filtering-CPL'];
+
+                                // Membuat pernyataan if berdasarkan nilai dropdown
+                                if ($selectedValue == 'TF-01') {
+                                    $kode_cpl = 'TF-01';
+                                } elseif ($selectedValue == 'TF-02') {
+                                    $kode_cpl = 'TF-02';
+                                } elseif ($selectedValue == 'TF-03') {
+                                    $kode_cpl = 'TF-03';
+                                } elseif ($selectedValue == 'TF-04') {
+                                    $kode_cpl = 'TF-04';
+                                } elseif ($selectedValue == 'TF-05') {
+                                    $kode_cpl = 'TF-05';
+                                } elseif ($selectedValue == 'TF-06') {
+                                    $kode_cpl = 'TF-06';
+                                } elseif ($selectedValue == 'TF-07') {
+                                    $kode_cpl = 'TF-07';
+                                } elseif ($selectedValue == 'TF-08') {
+                                    $kode_cpl = 'TF-08';
+                                } elseif ($selectedValue == 'TF-09') {
+                                    $kode_cpl = 'TF-09';
+                                } elseif ($selectedValue == 'TF-10') {
+                                    $kode_cpl = 'TF-10';
+                                } 
+                            }   
+
+                            $query = $conn->prepare("SELECT kelas_nilaicpmk.*, ikcpl.id_ikcpl, mhsw.tahun, mhsw.nama, ikcpl.id_cpl 
+                            FROM kelas_nilaicpmk 
                             JOIN kelas_cpmk ON kelas_nilaicpmk.id_cpmk = kelas_cpmk.id_cpmk 
                             JOIN ikcpl ON ikcpl.id_ikcpl = kelas_cpmk.id_ikcpl 
-                            JOIN mhsw ON kelas_nilaicpmk.nrp_hash = mhsw.nrp_hash
-                            WHERE ikcpl.id_cpl = 'TF-01' 
-                            AND kelas_nilaicpmk.nilai < (
-                                SELECT AVG(nilai) 
-                                FROM kelas_nilaicpmk
-                                JOIN kelas_cpmk ON kelas_nilaicpmk.id_cpmk = kelas_cpmk.id_cpmk 
-                                JOIN ikcpl ON ikcpl.id_ikcpl = kelas_cpmk.id_ikcpl 
-                                WHERE ikcpl.id_cpl = 'TF-01' 
-                                GROUP BY ikcpl.id_cpl
-                            )");
+                            JOIN mhsw ON kelas_nilaicpmk.nrp_hash = mhsw.nrp_hash 
+                            WHERE ikcpl.id_cpl = 'TF-01' AND kelas_nilaicpmk.nilai < 
+                                (SELECT AVG(nilai) 
+                                 FROM kelas_nilaicpmk 
+                                 JOIN kelas_cpmk ON kelas_nilaicpmk.id_cpmk = kelas_cpmk.id_cpmk 
+                                 JOIN ikcpl ON ikcpl.id_ikcpl = kelas_cpmk.id_ikcpl
+                                 WHERE ikcpl.id_cpl = 'TF-01' 
+                                 GROUP BY ikcpl.id_cpl )");
+
                             $query->execute();
                             $rowNum = 1; 
                             while($row = $query->fetch()) {
@@ -83,7 +173,7 @@ include ("../api/connect.php");
                                 <td>'.$row['nrp_hash'].'</td>
                                 <td>'.$row['nama'].'</td>
                                 <td>'.$row['nilai'].'</td>
-                                <td>'.$row['id_ikcpl'].'</td>
+                                <td>'.$row['id_cpl'].'</td>
                                 <td>'.$row['tahun'].'</td>
                             </tr>';
                             $rowNum++; 
