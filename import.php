4c8d1e32fd310c6ba2bb42e95219acdf476c0c9e
felinaevangelica <c14210136@john.petra.ac.id>
2023-12-31 23:53:48 +0700

    finalisasi ui import

--- import.php
+++ import.php
@@ -1,5 +1,5 @@
 <?php
-include("../api/connect.php");
+include 'api/connect.php';
 
 if (isset($_POST["import"])) {
     $filename = $_FILES["file"]["tmp_name"];
@@ -84,9 +84,72 @@ if (isset($_POST["import"])) {
 }
 ?>
 
-<form class="form-horizontal" action="" method="post" name="uploadcsv" enctype="multipart/form-data">
-    <div>
-        <input type="file" name="file" accept=".csv">
-        <button type="submit" name="import">Import</button>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Import</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+        <link rel="stylesheet" type="text/css" href="css.css">
+
+    <!-- lock screen, spy tdk bisa di swipe kanan kiri -->
+    <style>
+        body {
+            overflow-x: hidden;
+        }
+
+        .row {
+            display: inline;
+            margin-right: 20px;
+            margin-top: 10px;
+            display: flex;
+            flex:1;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+
+        .import-btn {
+            margin-top: 20px;
+
+        }
+    </style>
+</head>
+
+<body>
+    <!-- navbar -->
+    <?php include "navbar/navbar_import.php";?>
+
+    <div class="container">
+        <div class="row">
+            <form class="form-horizontal" action="" method="post" name="uploadcsv" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <label class="custom-label" for="file">Choose File</label>
+                            </div>
+                            <div class="col-md-2">:
+                            </div>
+
+                            <div class="col-md-4">
+                                <input type="file" name="file" accept=".csv" class="file-input">
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            <button type="submit" name="import" class="btn import-btn">Import</button>
+                        </div>
+                </form>
+        </div>
+            
     </div>
-</form>
+</body>
--- navbar/navbar_import.php
+++ navbar/navbar_import.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="../css.css">
+        <link rel="stylesheet" type="text/css" href="css.css">
     </head>
 
     <body>
@@ -35,19 +35,20 @@
                 </div>
             </nav>
         </div> -->
-
+        
+        <!-- DIBUAT KARENA KALAU PAKAI NAVBAR AFTER LOGIN BIASANYA GABISA GET SRC IMG NYA -->
         <nav class="navbar navbar-expand-lg navbar fixed-top">
             <a class="navbar-brand" href="#">
-                <img src="../assets/pcu_black.png" alt="Logo" width="100" height="100" class="logo-img">
+                <img src="assets/pcu_black.png" alt="Logo" width="100" height="100" class="logo-img">
             </a>
             <a class="navbar-brand" href="#">
-                <img src="../assets/pcu_blue.png" alt="Logo" width="150" height="100" class="logo-img">                    
+                <img src="assets/pcu_blue.png" alt="Logo" width="150" height="100" class="logo-img">                    
             </a>
             <ul id="nav" class="nav navbar-nav navbar-right">
-                <li><a href="../import.php">IMPORT</a></li>
-                <li><a href="../cpl/home_cpl.php">CPL</a></li>
-                <li><a href="../ipk/home_ipk.php">IPK</a></li>
-                <li><a href="../welcome_page.php">LOG OUT</a></li>                
+                <li><a href="import.php">IMPORT</a></li>
+                <li><a href="cpl/home_cpl.php">CPL</a></li>
+                <li><a href="ipk/home_ipk.php">IPK</a></li>
+                <li><a href="welcome_page.php">LOG OUT</a></li>                
             </ul>
         </nav>
     </body>
